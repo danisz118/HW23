9d4b97b887deed57459bd7967ee9bdb0ab3e843e
danisz118 <daniz118@gmail.com>
2020-04-18 10:26:45 +0300

    add gson

--- src/main/java/com/zacharov/api/TestApi.java
+++ src/main/java/com/zacharov/api/TestApi.java
@@ -1,6 +1,8 @@
 package com.zacharov.api;
 
 
+import com.google.gson.Gson;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
@@ -10,9 +12,10 @@ public class TestApi {
     @GET
     public Response getUserInJSON() {
         User user = new User();
+        Gson gson = new Gson();
         user.setAge(22);
         user.setName("Den");
-        String result = "User saved : " + user;
+        String result = gson.toJson(user);
         return Response.status(200).entity(result).build();
 
     }
